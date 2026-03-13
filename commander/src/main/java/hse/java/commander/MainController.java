4574794c1491c50d26fa7bdb08a77e4435f1d19b
Matvey Bazhenov <bazhenov.matvej@bk.ru>
2026-03-13 21:19:24 +0300

    commander: solution

--- commander/src/main/java/hse/java/commander/MainController.java
+++ commander/src/main/java/hse/java/commander/MainController.java
@@ -4,18 +4,112 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class MainController {
 
     @FXML
     public ListView<String> left;
-
     @FXML
     public ListView<String> right;
-
     @FXML
     public Button move;
+    @FXML
+    public Button delete;
+    @FXML
+    public Button copy;
+
+    private ListView<String> activePanel;
+
+    private Path panelTasks(Path nameDir, ListView<String> panel) {
+        int index = panel.getSelectionModel().getSelectedIndex();
+        if (index > 0) {
+            File f = new File(nameDir.toFile().getAbsolutePath(), panel.getItems().get(index));
+            if (f.isDirectory()) {
+                nameDir = nameDir.resolve(panel.getItems().get(index));
+                moveAction(nameDir, panel);
+            }
+        }
+        if (index == 0) {
+            nameDir = backAction(nameDir, panel);
+        }
+        return nameDir;
+    }
+
+    private void moveAction(Path nameDir, ListView<String> panel) {
+        panel.getItems().clear();
+        panel.getItems().add("...");
+        for (File file : nameDir.toFile().listFiles()) {
+            if (!file.isHidden()) {
+                panel.getItems().add(file.getName());
+            }
+        }
+    }
+
+    private Path backAction(Path nameDir, ListView<String> panel) {
+        System.out.println(nameDir.getParent());
+        nameDir = nameDir.getParent();
+        if (nameDir.equals(nameDir.getRoot())) {
+            throw new RuntimeException("Выше директории нет");
+        }
+        moveAction(nameDir, panel);
+        return nameDir;
+    }
+
+    void copy(Path nameDir, ListView<String> panel, Path toNameDir, ListView<String> toPanel) {
+        int index = panel.getSelectionModel().getSelectedIndex();
+        if (index > 0) {
+            File f = new File(nameDir.toFile().getAbsolutePath(), panel.getItems().get(index));
+            if (f.isFile()) {
+                if (f.isFile()) {
+                    try {
+                        Files.copy(nameDir.resolve(f.getName()), toNameDir.resolve(f.getName()));
+                        moveAction(toNameDir, toPanel);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+
+    void move(Path nameDir, ListView<String> panel, Path toNameDir, ListView<String> toPanel) {
+        int index = panel.getSelectionModel().getSelectedIndex();
+        if (index > 0) {
+            File f = new File(nameDir.toFile().getAbsolutePath(), panel.getItems().get(index));
+            if (f.isFile()) {
+                if (f.isFile()) {
+                    try {
+                        Files.move(nameDir.resolve(f.getName()), toNameDir.resolve(f.getName()));
+                        moveAction(toNameDir, toPanel);
+                        moveAction(nameDir, panel);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+
+    void delete(Path nameDir, ListView<String> panel) {
+        int index = panel.getSelectionModel().getSelectedIndex();
+        if (index > 0) {
+            File f = new File(nameDir.toFile().getAbsolutePath(), panel.getItems().get(index));
+            if (f.isFile()) {
+                if (f.isFile()) {
+                    try {
+                        Files.delete(nameDir.resolve(f.getName()));
+                        moveAction(nameDir, panel);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
 
     private Path leftDir;
     private Path rightDir;
@@ -27,19 +121,60 @@ public class MainController {
     }
 
     public void initialize() {
-        move.setOnMouseClicked(event -> {
-
-        });
-        System.out.println(System.getProperty("user.home"));
-        left.getItems().add("Kek");
+        leftDir = Path.of(System.getProperty("user.home"));
+        rightDir = Path.of(System.getProperty("user.home"));
+        System.out.println(leftDir.toFile().getAbsoluteFile());
+        System.out.println(leftDir.toFile().getAbsolutePath());
+        moveAction(leftDir, left);
+        moveAction(rightDir, right);
 
         left.setOnMouseClicked(event -> {
+            activePanel = left;
             if (event.getClickCount() == 2) {
-                int index = left.getSelectionModel().getSelectedIndex();
-                if (index >= 0) {
-                    left.getItems().set(index, "clicked");
+                leftDir = panelTasks(leftDir, left);
+            }
+        });
+
+        right.setOnMouseClicked(event -> {
+            activePanel = right;
+            if (event.getClickCount() == 2) {
+                rightDir = panelTasks(rightDir, right);
+            }
+        });
+
+        copy.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 1) {
+                if (activePanel == left) {
+                    copy(leftDir, left, rightDir, right);
+                }
+                if (activePanel == right) {
+                    copy(rightDir, right, leftDir, left);
                 }
             }
         });
+
+        move.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 1) {
+                if (activePanel == left) {
+                    move(leftDir, left, rightDir, right);
+                }
+                if (activePanel == right) {
+                    move(rightDir, right, leftDir, left);
+                }
+            }
+        });
+
+        delete.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 1) {
+                if (activePanel == left) {
+                    delete(leftDir, left);
+                }
+                if (activePanel == right) {
+                    delete(rightDir, right);
+                }
+            }
+        });
+
+
     }
 }
